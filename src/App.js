cd0d7b8c4891e4378f394cb3306a58f6a2e1c9b1
cem <cemyilmaztech@gmail.com>
2024-09-02 12:31:46 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import "./App.css"
+import CardContainer from "./components/CardContainer";
+import MyFooter from "./components/MyFooter";
+import MyNavbar from "./components/MyNavbar";
+import {data} from "./helper/data"
+
 function App() {
+  const [search, setSearch] = useState("");
+  const handleChange=(searchTerm)=>setSearch(searchTerm);
   return (
     <div className="App">
-    
+    <MyNavbar onChange={handleChange} />
+    <CardContainer data={data} search={search} />
+    <MyFooter/>
     </div>
   );
 }
